90f5563c7de406603cb11d4116d8b0376823f465
Fangcong Yin <fyin2@nd.edu>
2020-03-27 20:05:00 -0400

    Fangcong Yin

--- app/src/main/java/com/example/yinfa/myflashcard/MainActivity.java
+++ app/src/main/java/com/example/yinfa/myflashcard/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.yinfa.myflashcard;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 import java.util.List;
@@ -24,14 +28,35 @@ public class MainActivity extends AppCompatActivity {
             ((TextView)findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
         }
         findViewById(R.id.Next).setOnClickListener(new View.OnClickListener(){
+
             public void onClick(View v){
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.left_out);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(v.getContext(), R.anim.right_in);
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
                 currentCardDisplayedIndex ++;
                 if ( currentCardDisplayedIndex > allFlashcards.size()-1){
                     currentCardDisplayedIndex = 0;
                 }
+                findViewById(R.id.flashcard_question).startAnimation(leftOutAnim);
                 ((TextView)findViewById(R.id.answer)).setText(allFlashcards.get(currentCardDisplayedIndex).getAnswer());
                 ((TextView)findViewById(R.id.flashcard_question)).setText(allFlashcards.get(currentCardDisplayedIndex).getQuestion());
-
+                findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
 
             }
         });
@@ -39,8 +64,20 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
-                findViewById(R.id.answer).setVisibility(View.VISIBLE);
+                View answerSideView = findViewById(R.id.answer);
+
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+
+                float finalRadius = (float) Math.hypot(cx, cy);
+
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
+
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                answerSideView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(500);
+                anim.start();
             }
         });
 
@@ -59,6 +96,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                     startActivityForResult(intent, 450);
+                    overridePendingTransition(R.anim.right_in, R.anim.left_out);
                 }
             });
 
